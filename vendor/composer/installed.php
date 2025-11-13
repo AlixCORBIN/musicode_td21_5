de417118e577e9b40f5bbf05372f558587716e58
Foucher <marc@MacBook-Air-de-Foucher.local>
2025-11-13 12:26:55 +0100

    page de base

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vlucas/phpdotenv',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4ee76689df8207d10bc0d13b2acd4ac5f5f446ac',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vlucas/phpdotenv' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4ee76689df8207d10bc0d13b2acd4ac5f5f446ac',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ view/home.php
@@ -0,0 +1,32 @@
+<?php
+$title = "Musicode";
+$description = "Aucune description";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $title; ?></title>
+  <link rel="stylesheet" href="/musicode_td21_5/style.css">
+</head>
+<body>
+  <header>
+    <div class="brand">
+        <img src="Musicode-1.svg" alt="Logo Musicode" width="100">
+        <h1><?php echo $title; ?></h1>
+    </div>
+    
+
+    <nav>
+        <a href="#">Catalogue</a>
+        <a href="#">Connexion</a>
+        <a href="#">Inscription</a>
+    </nav>
+
+  </header>
+    <footer>
+      <p>© 2025 Musicode · IUT Laval - R3.01 Développement web 2025-2026.</p>
+    </footer>
+  </main>
+</body>
+</html>
