5ad6ed95e5ac8e8c7e34074420921ef03871c321
Marc <marcfchr.72@gmail.com>
2025-12-01 10:44:30 +0100

    enlever ce qui sert a r

--- core/View.php
+++
@@ -1,8 +0,0 @@
-<?php
-class View {
-    public static function render($viewName, $data = []) {
-        extract($data);
-        require_once __DIR__ . '/../app/views/' . $viewName . '.php';
-    }
-}
-?>
