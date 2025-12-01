46bb774f3589b8b28cf180ced2c6b4f88457744f
Marc <marcfchr.72@gmail.com>
2025-12-01 10:50:27 +0100

    dossier css

--- controller/homeController.php
+++ controller/homeController.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . '/../core/Controller.php';
 require_once __DIR__ . '/../core/Model.php';
-require_once __DIR__ . '/../core/View.php';
 
 class HomeController extends Controller {
     public function index() {
